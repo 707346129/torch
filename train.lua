8ce20307fc061eaa784d19c5db97b7ce0b11f0c2
Xue Zhendong <707346129@qq.com>
2018-02-05 18:07:48 +0800

    train.lua

---
+++ train.lua
@@ -0,0 +1,14 @@
+#!/usr/local/bin/lua
+
+
+require "torch";
+require "nn";
+require "network.lua";
+input = torch.rand(1,32,32)
+output = net:forward(input)
+print(output)
+net:zeroGradParameters();
+--gradInput = net:backward(input, torch.rand(10))
+
+--print(#gradInput)
+
