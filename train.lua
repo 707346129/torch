39f993ab84e08b930f5ba2b7593b1bba54e5f946
Xue Zhendong <707346129@qq.com>
2018-02-05 20:22:37 +0800

    train 中 require修改

--- train.lua
+++ train.lua
@@ -3,12 +3,12 @@
 
 require "torch";
 require "nn";
-require "network.lua";
+require "network";
 input = torch.rand(1,32,32)
 output = net:forward(input)
 print(output)
 net:zeroGradParameters();
---gradInput = net:backward(input, torch.rand(10))
+gradInput = net:backward(input, torch.rand(10))
 
 --print(#gradInput)
 
