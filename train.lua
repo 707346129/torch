1c60507757e06c02be8d58f7e178dcd093c43e25
Xue Zhendong <707346129@qq.com>
2018-02-06 14:21:26 +0800

    10 -> 3

--- train.lua
+++ train.lua
@@ -12,3 +12,10 @@ gradInput = net:backward(input, torch.rand(10))
 
 print(#gradInput)
 
+criterion = nn.ClassNLLCriterion() --损失函数
+criterion:forward(output, 3) --3 为正确的 label
+gradients = criterion:backward(output, 3)
+
+gradInput = net:backward(input, gradients) --在最后一层后加入损失函数后重新计算grad
+
+net:updateParameters(0.01)
